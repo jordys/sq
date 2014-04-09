f4c8ddaba8af63f7ad13d9d64c3bd1edf67b5025
jshute <jordan.shute@icloud.com>
2014-04-09 10:10:51 -0700

    Fixed the debugger to not emit PHP notices. Kinda ironic when you think about it.

--- views/debug.php
+++ views/debug.php
@@ -3,7 +3,16 @@
 <? if (isset($error['line'])): ?>
 	<p><strong>Line #<?=$error['line']?></strong> <?=$error['string']?> in <?=$error['file']?></p>
 	<h2>Trace</h2>
-	<? foreach ($error['trace'] as $line): ?>
+	<? foreach ($error['trace'] as $line):
+		if (!isset($line['class'])):
+			$line['class'] = null;
+		endif;
+		
+		if (!isset($line['type'])):
+			$line['type'] = null;
+		endif;
+		
+		?>
 		<p><strong>Line #<?=$line['line']?></strong> <?=$line['file']?> <?=$line['class'],$line['type'],$line['function']?></p>
 	<? endforeach ?>
 <? endif ?>
