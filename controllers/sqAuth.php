ab1d38984784b08b7572d9280796c83e1eb94986
Jordan Shute <jordan.shute@icloud.com>
2014-12-07 15:19:33 -0700

    Refactoring

--- controllers/sqAuth.php
+++ controllers/sqAuth.php
@@ -17,11 +17,10 @@ abstract class sqAuth extends controller {
 	
 	// Start session on object creation
 	public function init() {
-		self::startSession();
+		self::session();
 	}
 	
-	// Helper to make sure session is started
-	private static function startSession() {
+	private static function session() {
 		if (!isset($_SESSION)) {
 			session_start();
 		}
@@ -62,12 +61,6 @@ abstract class sqAuth extends controller {
 		sq::redirect($_SERVER['HTTP_REFERER']);
 	}
 	
-	// Checks a password against a hashed password
-	public static function authenticate($password, $hash) {
-		$hasher = new PasswordHash(8, sq::config('auth/portable-hashes'));
-		return $hasher->checkPassword($password, $hash);
-	}
-	
 	// Log out action
 	public function logoutAction() {
 		self::logout();
@@ -75,29 +68,22 @@ abstract class sqAuth extends controller {
 		sq::redirect($_SERVER['HTTP_REFERER']);
 	}
 	
-	// Logs the current user out of the system
-	public static function logout() {
-		self::startSession();
-		
-		$_SESSION['sq-level'] = null;
-		$_SESSION['sq-username'] = null;
-		$_SESSION['sq-login-attempts'] = 0;
-		
-		// Clear the cookie
-		setcookie('auth', null, time() - 10, '/');
+	// Checks a password against a hashed password
+	public static function authenticate($password, $hash) {
+		$hasher = new PasswordHash(8, sq::config('auth/portable-hashes'));
+		return $hasher->checkPassword($password, $hash);
 	}
 	
 	// Logs the user with the username passed in into the system and sets a 
 	// session. If the remember argument is true and the remember-me option is 
 	// true a cookie will be set as well.
 	public static function login($username, $remember = false) {
-		self::startSession();
+		self::session();
 		
-		$user = sq::model('users');
-		$user->options['load-relations'] = false;
-		$user->where(array(sq::config('auth/username-field') => $username));
-		$user->limit();
-		$user->read();
+		$user = sq::model('users', array('load-relations' => false))
+			->where(array(sq::config('auth/username-field') => $username))
+			->limit()
+			->read();
 		
 		// Set the user info to the session
 		$_SESSION['sq-username'] = $user->{sq::config('auth/username-field')};
@@ -107,11 +93,9 @@ abstract class sqAuth extends controller {
 		if ($remember && sq::config('auth/remember-me')) {
 			$timeout = time() + sq::config('auth/cookie-timeout');
 			
-			$created = strtotime($user->created);
-			
 			// A hashkey is saved to the user and into a cookie. If these two
 			// parameters match the user will be allowed to log in.
-			$hash = $user->{sq::config('auth/username-field')}.$user->password.$created.sq::config('auth/salt');
+			$hash = $user->{sq::config('auth/username-field')}.$user->password;
 			
 			setcookie('auth', self::hash($hash), $timeout, '/');
 			
@@ -120,48 +104,11 @@ abstract class sqAuth extends controller {
 		}
 	}
 	
-	// Returns a model object of the currently logged in user with optional
-	// recursion.
-	public static function user($loadRelations = false) {
-		if (is_object(self::$user)) {
-			return self::$user;
-		} else {
-			self::startSession();
-			
-			$user = sq::model('users');
-			$user->options['load-relations'] = $loadRelations;
-			$user->limit();
-			
-			// Check session for login
-			if (isset($_SESSION['sq-username'])) {
-				$user->where(array(sq::config('auth/username-field') => $_SESSION['sq-username']));
-				$user->read();
-			
-			// If no session than check for a cookie if cookie login is enabled
-			} elseif (isset($_COOKIE['sq-auth']) && sq::config('auth/remember-me')) {
-				$user->where(array('hashkey' => $_COOKIE['sq-auth']));
-				$user->read();
-				
-				// If a user is found log the user in again to increase the length
-				// of the cookie
-				if (isset($user->level)) {
-					self::login($user->{sq::config('auth/username-field')}, true);
-				}
-			}
-			
-			// Return the user object if it exists
-			if (isset($user->level)) {
-				self::$user = $user;
-				return $user;
-			}
-		}
-	}
-	
 	// Checks if the currently logged in users level. Returns true if user level
 	// matches. If no argument is passed the function will return true for any
 	// level.
 	public static function check($level = true) {
-		self::startSession();
+		self::session();
 		
 		// Check session
 		if (isset($_SESSION['sq-level']) && $level == $_SESSION['sq-level']) {
@@ -187,6 +134,50 @@ abstract class sqAuth extends controller {
 		return false;
 	}
 	
+	// Logs the current user out of the system
+	public static function logout() {
+		self::startSession();
+		
+		$_SESSION['sq-level'] = null;
+		$_SESSION['sq-username'] = null;
+		$_SESSION['sq-login-attempts'] = 0;
+		
+		// Clear the cookie
+		setcookie('auth', null, time() - 10, '/');
+	}
+	
+	// Returns a model object of the currently logged in user with optional
+	// recursion.
+	public static function user($options = array('load-relations' => false)) {
+		if (is_object(self::$user)) {
+			return self::$user;
+		}
+		
+		self::session();
+		
+		$user = sq::model('users', $options)
+			->limit();
+		
+		// Check session for login
+		if (isset($_SESSION['sq-username'])) {
+			$user->where(array(sq::config('auth/username-field') => $_SESSION['sq-username']));
+			$user->read();
+			
+		// If no session than check for a cookie if cookie login is enabled
+		} elseif (isset($_COOKIE['sq-auth']) && sq::config('auth/remember-me')) {
+			$user->where(array('hashkey' => $_COOKIE['sq-auth']));
+			$user->read();
+			
+			// If a user is found log the user in again to increase the length
+			// of the cookie
+			if (isset($user->level)) {
+				self::login($user->{sq::config('auth/username-field')}, true);
+			}
+		}
+		
+		return $user;
+	}
+		
 	// Returns hashed password
 	public static function hash($password) {
 		$hasher = new PasswordHash(8, sq::config('auth/portable-hashes'));
@@ -197,35 +188,26 @@ abstract class sqAuth extends controller {
 	// This function is a special action for the admin module that handles the
 	// changing of passwords in the admin interface
 	public function passwordAction() {
-		$flash = null;
-		
-		if (!sq::config('admin/require-login') || auth::check('admin')) {
-			$users = sq::model(url::get('model'));
-			$users->options['load-relations'] = false;
-			$users->where(url::get('id'));
-			$users->read();
-			
-			if (url::post()) {
-				if (url::post('password') == url::post('confirm')) {
-					$users->password = self::hash(url::post('password'));
-					$users->update();
-					
-					sq::redirect(sq::base().'admin/'.url::get('model'));
-				} else {
-					$flash = 'Passwords don&rsquo;t match';
-				}
-				
-				return sq::view('admin/forms/password', array(
-					'model' => $users
-				));
-			} else {
-				return sq::view('admin/forms/password', array(
-					'model' => $users
-				));
-			}
-		} else {
-			return sq::view('admin/login', $flash);
+		if (sq::config('admin/require-login') || !self::check('admin')) {
+			return sq::view('admin/login');
 		}
+		
+		$users = sq::model(url::get('model'), array('load-relations' => false))
+			->where(url::get('id'))
+			->read();
+			
+		if (url::post()) {
+			if (url::post('password') == url::post('confirm')) {
+				$users->password = self::hash(url::post('password'));
+				$users->update();
+				
+				sq::redirect(sq::base().'admin/'.url::get('model'));
+			}
+		}
+			
+		return sq::view('admin/forms/password', array(
+			'model' => $users
+		));
 	}
 }
 
