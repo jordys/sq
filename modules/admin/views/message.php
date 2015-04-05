7b731a369595a0e25e6c190a900afa314d99352e
jshute <jordan.shute@icloud.com>
2015-04-04 23:36:04 -0700

    Moved admin module to different repo

--- modules/admin/views/message.php
+++
@@ -1,7 +0,0 @@
-<section class="message">
-	<h2><?php echo $message?></h2>
-	<?php if (isset($description)): ?>
-		<p><?php echo $description?></p>
-	<?php endif ?>
-	<a href="<?php echo $base.'admin/'.url::get('controller').$modelName?>">Back</a>
-</section>
